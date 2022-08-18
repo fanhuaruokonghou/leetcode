79b42020b284bd40ed9113be6c60324a76c1db0c
fanhuaruokonghou <3184107533@qq.com>
2022-08-18 22:23:31 +0800

    Update 338

--- DynamicProgramming/338.go
+++ DynamicProgramming/338.go
@@ -1,6 +1,6 @@
 package DynamicProgramming
 
-func countBits(n int) []int {
+func countBits338(n int) []int {
 	dp := make([]int, n+1)
 	for i := 1; i < n+1; i++ {
 		j := i
@@ -10,6 +10,14 @@ func countBits(n int) []int {
 			}
 			j = j >> 1
 		}
+	}
+	return dp
+}
+
+func countBits338Method2(n int) []int {
+	dp := make([]int, n+1)
+	for i := 1; i < n+1; i++ {
+		dp[i] = dp[i>>1] + i&1
 	}
 	return dp
 }
